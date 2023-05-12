af515ab10a72bdaf1582c1b8c4a59a1c15b5c17f
atulmisrajob <atulmisrajob@gmail.com>
2023-05-12 12:28:29 +0530

    initial

---
+++ src/main/java/com/example/atulbrewery/AtulBreweryApplication.java
@@ -0,0 +1,13 @@
+package com.example.atulbrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtulBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtulBreweryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/atulbrewery/AtulBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atulbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtulBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
